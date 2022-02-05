b32a2ee273af0b74642807747e5df6f8e4885716
Dorah-dev <69757680+Dorah-dev@users.noreply.github.com>
2022-02-06 01:29:12 +0200

    Create index-paging.blade.php

---
+++ resources/views/admin/products/index-paging.blade.php
@@ -0,0 +1,51 @@
+@extends('layout')
+
+@section('content')
+<h1>Products</h1>
+
+<p>
+    <a class="btn btn-primary" href="/products/create"><span class="glyphicon glyphicon-plus"></span> Add Product</a>
+</p>
+
+<table class="table table-bordered table-hover">
+    <thead>
+        <th>Category</th>
+        <th>Name</th>
+        <th>SKU</th>
+        <th>Price</th>
+        <th>Actions</th>
+    </thead>
+    <tbody>
+        @if ($products->count() == 0)
+        <tr>
+            <td colspan="5">No products to display.</td>
+        </tr>
+        @endif
+
+        @foreach ($products as $product)
+        <tr>
+            <td>{{ $product->category->name }}</td>
+            <td>{{ $product->name }}</td>
+            <td>{{ $product->sku }}</td>
+            <td>${{ $product->price }}</td>
+            <td>
+                <a class="btn btn-sm btn-success" href="{{ action('ProductsController@edit', ['id' => $product->id]) }}">Edit</a>
+
+                <form style="display:inline-block" action="{{ action('ProductsController@destroy', ['id' => $product->id]) }}" method="POST">
+                    @method('DELETE')
+                    @csrf
+                    <button class="btn btn-sm btn-danger"> Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
+{{ $products->links() }}
+
+<p>
+    Displaying {{$products->count()}} of {{ $products->total() }} product(s).
+</p>
+
+@endsection
